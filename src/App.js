0338010df41a3f99b584c93fdaa46729bb35c3d1
Felix LE DREF <felix.ledref@gmail.com>
2021-08-31 13:18:12 +0200

    wip add polyjuice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
 import './App.css'
-import { WEB3_PROVIDER_URL, ROLLUP_TYPE_HASH, ETH_ACCOUNT_LOCK_CODE_HASH, CONTRACT_ABI, CONTRACT_ADDR } from './config'
+import { WEB3_PROVIDER_URL, ROLLUP_TYPE_HASH, ETH_ACCOUNT_LOCK_CODE_HASH, CONTRACT_ADDR, CONTRACT_ABI  } from './config'
 import { PolyjuiceHttpProvider } from '@polyjuice-provider/web3';
 import Home from './Home'
+import { AddressTranslator } from 'nervos-godwoken-integration';
 
 class App extends Component {
   componentWillMount() {
@@ -11,17 +12,20 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    // const godwokenRpcUrl = WEB3_PROVIDER_URL;
-    // const providerConfig = {
-    //     rollupTypeHash: ROLLUP_TYPE_HASH,
-    //     ethAccountLockCodeHash: ETH_ACCOUNT_LOCK_CODE_HASH,
-    //     web3Url: godwokenRpcUrl
-    // };
-    // const provider = new PolyjuiceHttpProvider(godwokenRpcUrl, providerConfig);
-    // const web3 = new Web3(provider);
-    const web3 = new Web3("http://127.0.0.1:9545");
+    const godwokenRpcUrl = WEB3_PROVIDER_URL;
+    const providerConfig = {
+        rollupTypeHash: ROLLUP_TYPE_HASH,
+        ethAccountLockCodeHash: ETH_ACCOUNT_LOCK_CODE_HASH,
+        web3Url: godwokenRpcUrl
+    };
+    const provider = new PolyjuiceHttpProvider(godwokenRpcUrl, providerConfig);
+    const web3 = new Web3(provider);
+    // const web3 = new Web3("http://127.0.0.1:9545");
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
+    const addressTranslator = new AddressTranslator();
+    const polyjuiceAddress = addressTranslator.ethAddressToGodwokenShortAddress(accounts[0]);
+    this.setState({polyjuiceAddress: polyjuiceAddress})
     const simplePoll = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDR)
     this.setState({ simplePoll })
     const pollCount = await simplePoll.methods.pollCount().call()
@@ -32,6 +36,7 @@ class App extends Component {
         polls: [...this.state.polls, poll]
       })
     }
+    this.setState({ loading: false })
   }
 
   constructor(props) {
@@ -39,15 +44,20 @@ class App extends Component {
     this.createPoll = this.createPoll.bind(this)
     this.state = {
       account: '',
+      polyjuiceAddress: '',
       pollCount: 0,
-      polls: []
+      polls: [],
+      loading: true
     }
   }
 
   createPoll(content) {
+    this.setState({ loading: true })
     this.state.simplePoll.methods.createPoll(content).send({ from: this.state.account, gas: 6000000 })
     .once('receipt', (receipt) => {
       console.log("poll created")
+      this.setState({ loading: false })
+      window.location.reload()
     })
   }
 
@@ -56,6 +66,7 @@ class App extends Component {
       <div className="container-fluid">
         <h1>Simple Poll Dapp</h1>
         <p>Your account: {this.state.account}</p>
+        <p>Polyjuice address: {this.state.polyjuiceAddress}</p>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex justify-content-center">
               { this.state.loading
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const WEB3_PROVIDER_URL = "https://godwoken-testnet-web3-rpc.ckbapp.dev";
 export const ROLLUP_TYPE_HASH = "0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a";
 export const ETH_ACCOUNT_LOCK_CODE_HASH = "0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22";
-export const CONTRACT_ADDR = "0x4CfCDdE1eDBB3feE89Ddc7e9636249c4A4d9Cf1b";
+export const CONTRACT_ADDR = "0x438b8501e51e984C148EB95772989a6Befe984f8";
 export const CONTRACT_ABI = [
   {
     "inputs": [],
